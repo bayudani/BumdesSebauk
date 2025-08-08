60fde3c8e33b4ef405d7cae4737226c364845c4a
nvyaantii <noviiyanntii@email.com>
2025-08-08 11:09:32 +0700

    product

--- routes/web.php
+++ routes/web.php
@@ -4,11 +4,8 @@ use App\Livewire\Article\ArticleDetail;
 use Illuminate\Support\Facades\Route;
 use Stephenjude\FilamentBlog\Models\Post;
 
-Route::view('/', 'home')->name('home');
-Route::view('/berita', 'article')->name('articles');
-Route::view('/produk', 'article')->name('Produk');
-    // ->name('articles')
-    // ->middleware(['auth', 'verified']);
+Route::view('/', 'home');
+Route::view('/product', 'product');
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
